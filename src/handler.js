61d1356f4accb01817efffa57dd2bab50bad60d4
Abimdn <abimdanu@gmail.com>
2024-05-06 10:02:27 +0700

    Fix bugs: Invalid update date

--- src/handler.js
+++ src/handler.js
@@ -63,7 +63,7 @@ const getNoteByIdHandler = (request, h) => {
 const editNoteByIdHandler = (request, h) => {
   const { id } = request.params;
   const { title, tags, body } = request.payload;
-  const updatedAt = new Date().toISOString;
+  const updatedAt = new Date().toISOString();
 
   const index = notes.findIndex((n) => n.id === id);
 
